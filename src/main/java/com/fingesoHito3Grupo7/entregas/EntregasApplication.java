b382e671d428fb9f0991606a4b3eb607db4d6f23
Carla Astroza <carla.astroza@usach.cl>
2026-08-02 18:59:20 -0400

    Inicializando proyecto de entregas con Spring Boot y PostgreSQL

---
+++ src/main/java/com/fingesoHito3Grupo7/entregas/EntregasApplication.java
@@ -0,0 +1,13 @@
+package com.fingesoHito3Grupo7.entregas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregasApplication.class, args);
+	}
+
+}
